378f83622f3717faf5bd2e947b1eef8543519fc8
Kenton Lam <kenton_lam@outlook.com>
2018-05-23 17:16:42 +1000

    Added script to replace uq.jpeg

---
+++ src/blackboard_style_moderniser.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name        Blackboard Style Moderniser
+// @author      Kenton Lam
+// @description Improves some image assets on the site.
+// @match       https://learn.uq.edu.au/*
+// @version     0.1
+// ==/UserScript==
+
+(function() {
+    
+    let oldImg = document.querySelector('.brandingImgWrap .bannerImage');
+    oldImg.parentNode.removeChild(oldImg);
+
+    let logoLink = document.querySelector('.brandingImgWrap a');
+    logoLink.style.textDecoration = 'none';
+
+    let span = document.createElement('span');
+    span.style.display = 'table';
+    span.style.marginLeft = '9px';
+    span.style.marginTop = '19px';
+
+    let img = document.createElement('img');
+    img.src = 'https://static.uq.net.au/v3/logos/corporate/uq-logo-white.svg';
+    img.style.height = '44px';
+    img.style.paddingRight = '16px';
+    img.style.borderRight = 'white solid 1px';
+
+    span.appendChild(img);
+
+    let learnUQSpan = document.createElement('span');
+    learnUQSpan.style.display = 'table-cell';
+    learnUQSpan.style.verticalAlign = 'middle';
+    learnUQSpan.textContent = 'Learn.UQ';
+    learnUQSpan.style.fontFamily = '"Segoe UI", "Helvetica", sans-serif';
+    learnUQSpan.style.fontSize = '18pt';
+    learnUQSpan.style.paddingLeft = '18px';
+    learnUQSpan.style.color = 'white';
+    learnUQSpan.style.fontWeight = '400';
+    span.appendChild(learnUQSpan);
+
+    logoLink.appendChild(span);
+
+})();
